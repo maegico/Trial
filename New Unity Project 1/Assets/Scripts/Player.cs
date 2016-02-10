e3f76b21c28e9dcd9bfd7814631176b8cdf7299c
Sean Connolly (RIT Student) <spc1802@ad.rit.edu>
2016-02-10 14:53:04 -0500

    Made a simple unity project toplay with Git and Unity for class

---
+++ New Unity Project 1/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float horizontalVelocity = h * moveSpeed * Time.deltaTime;
+
+        float p = Input.GetAxis("Vertical");
+        float verticalVelocity = p * moveSpeed * Time.deltaTime;
+
+        Vector3 velocity = new Vector3(horizontalVelocity, verticalVelocity, moveSpeed * Time.deltaTime);
+
+        transform.Translate(velocity);
+
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+	}
+
+    void Shoot()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
---
+++ New Unity Project 1/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalVel = projectileSpeed * Time.deltaTime;
+        transform.Translate(horizontalVel, 0, 0);
+	}
+}
